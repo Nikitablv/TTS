6ebf860b4c8c08deae0e598b95bdfc207f52564c
Nikitablv <nik050502@yandex.ru>
2023-11-29 21:30:15 +0300

    Добавил миграцию создания БД

---
+++ TTS-26/00001_init.down.sql
@@ -0,0 +1,17 @@
+DROP TABLE employee_department;
+
+DROP TABLE employee_roles;
+
+DROP TABLE department;
+
+DROP TABLE employeeAccount;
+
+DROP TABLE tokens;
+
+DROP TABLE auth_user;
+
+DROP TABLE company;
+
+DROP TABLE role;
+
+DROP TYPE roleTypes;
